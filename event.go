da95aeacd0135be241b19cd772fe25a4f3f08976
dfreire <dario.freire@gmail.com>
2014-12-07 23:54:00 +0000

    update

--- event.go
+++ event.go
@@ -1,7 +1,12 @@
 package event
 
 import (
+	"errors"
+	"github.com/puffinframework/config"
 	"github.com/satori/go.uuid"
+	"github.com/syndtr/goleveldb/leveldb"
+	"log"
+	"os"
 	"time"
 )
 
@@ -21,8 +26,63 @@ func NewHeader(eventType string, version int) Header {
 	}
 }
 
+var (
+	ErrOpenStore    error = errors.New("snapshot: couldn't open store")
+	ErrCloseStore   error = errors.New("snapshot: couldn't close store")
+	ErrDestroyStore error = errors.New("snapshot: couldn't destroy store")
+)
+
 type Store interface {
 	ForEach(since time.Time, handler func(header Header))
 	MustLoad(header Header, data interface{})
 	MustSave(header Header, data interface{})
+	MustClose()
+	MustDestroy()
+}
+
+type leveldbStoreConfig struct {
+	EventStore struct {
+		LeveldbDir string
+	}
+}
+
+type leveldbStore struct {
+	dir string
+	db  *leveldb.DB
+}
+
+func NewLeveldbStore() Store {
+	cfg := &leveldbStoreConfig{}
+	config.MustReadConfig(cfg)
+
+	dir := cfg.EventStore.LeveldbDir
+
+	db, err := leveldb.OpenFile(dir, nil)
+	if err != nil {
+		log.Panic(ErrOpenStore)
+	}
+
+	return &leveldbStore{dir: dir, db: db}
+}
+
+func (self *leveldbStore) ForEach(since time.Time, handler func(header Header)) {
+}
+
+func (self *leveldbStore) MustLoad(header Header, data interface{}) {
+}
+
+func (self *leveldbStore) MustSave(header Header, data interface{}) {
+}
+
+func (self *leveldbStore) MustClose() {
+	if err := self.db.Close(); err != nil {
+		log.Panic(ErrCloseStore)
+	}
+}
+
+func (self *leveldbStore) MustDestroy() {
+	self.MustClose()
+	if err := os.RemoveAll(self.dir); err != nil {
+		log.Panic(ErrDestroyStore)
+	}
 }
