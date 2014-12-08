bf69bff54639fc44ba145271fbd769e18a65b805
dfreire <dario.freire@gmail.com>
2014-12-08 12:37:59 +0000

    update

--- event_test.go
+++ event_test.go
@@ -1,19 +1,28 @@
 package event_test
 
 import (
-	. "github.com/puffinframework/event"
+	"github.com/puffinframework/event"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestHeader(t *testing.T) {
-	header := NewHeader("EventType1", 3)
+	header := event.NewHeader("EventType1", 3)
 	assert.NotNil(t, header.CreatedAt)
 	assert.NotNil(t, header.ID)
+	assert.NotEqual(t, "", header.ID)
 	assert.Equal(t, "EventType1", header.Type)
 	assert.Equal(t, 3, header.Version)
 
-	encoded := MustEncodeEventHeader(header)
-	decoded := MustDecodeEventHeader(encoded)
+	encoded := event.MustEncodeEventHeader(header)
+	decoded := event.MustDecodeEventHeader(encoded)
 	assert.Equal(t, header, decoded)
+}
+
+func TestEventStore(t *testing.T) {
+	os.Setenv("PF_MODE", "test")
+	store := event.NewLeveldbStore()
+	assert.NotNil(t, store)
+	defer store.MustDestroy()
 }
